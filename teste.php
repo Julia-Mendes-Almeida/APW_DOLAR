13830d0ffd25d4a33417332c3dfed9c6183b2007
Julia-Mendes-Almeida <111704254+Julia-Mendes-Almeida@users.noreply.github.com>
2022-08-21 16:48:15 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -7,7 +7,7 @@
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            Julia Mendes
+ * Author:            Julia Mendes e Sofia
  * Author URI:        https://author.example.com/
  *
  */
